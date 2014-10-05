29bd00635f5ff3d2e6d4e8030f68b1e4abb5e0e5
jusjmkim <justinjmkim@gmail.com>
2014-10-05 18:22:10 -0400

    Start student model specs

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'Student Validation' do
+    before do
+      @student = Student.create(username: 'Justin', password: 'password')
+    end
+
+    it 'student is valid' do
+      expect(@student).to be_valid
+    end
+
+    it 'student has schedules' do
+    end
+  end
+
 end
