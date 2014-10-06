74ab064edc8c5893e3995c4022bb636286061c86
jusjmkim <justinjmkim@gmail.com>
2014-10-05 23:33:30 -0400

    Initial devise setup

--- db/migrate/20141005211951_create_students.rb
+++ db/migrate/20141005211951_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :username
-      t.string :password
 
       t.timestamps
     end
--- spec/features/schedule_features_spec.rb
+++ spec/features/schedule_features_spec.rb
@@ -2,10 +2,10 @@ require_relative '../rails_helper.rb'
 
 RSpec.describe 'Feature Test: Schedule Setup', type: :feature do
 
-  describe 'page renders correctly' do
-    before do
-      visit '/schedules'
-    end
-  end
+  # describe 'page renders correctly' do
+  #   before do
+  #     visit '/schedules'
+  #   end
+  # end
 
 end
