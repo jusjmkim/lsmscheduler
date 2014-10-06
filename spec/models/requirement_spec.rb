be497ae7735c08655d355a0c62bb24e1414261ab
jusjmkim <justinjmkim@gmail.com>
2014-10-05 22:23:49 -0400

    tests if major is valid

--- spec/models/requirement_spec.rb
+++ spec/models/requirement_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Requirement, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'Major Validation' do
+    it 'is valid' do
+      expect(@major).to be_valid
+    end
+  end
+
 end
