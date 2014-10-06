14f1f64e07b15206b5e75a19e9666ef37196c5e8
jusjmkim <justinjmkim@gmail.com>
2014-10-05 23:16:54 -0400

    Set up login page

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -7,12 +7,24 @@ RSpec.describe 'Feature Test: User Signup', type: :feature do
       visit '/'
     end
 
-    it 'has username field' do
-      expect(page).to have_content('Username')
+    context 'check headers' do
+      it 'has top header' do
+        expect(page).to have_content('LSM Scheduler')
+      end
+
+      it 'has second header' do
+        expect(page).to have_content('About')
+      end
     end
 
-    it 'has password field' do
-      expect(page).to have_content('Password')
+    context 'check login fields' do
+      it 'has username field' do
+        expect(page).to have_content('Username')
+      end
+
+      it 'has password field' do
+        expect(page).to have_content('Password')
+      end
     end
   end
 
