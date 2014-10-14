ddef45993d585d3cce23ea1cb079a28843e7831c
jusjmkim <justinjmkim@gmail.com>
2014-10-13 23:48:54 -0400

    start sessions testing

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe StudentsController, :type => :controller do
 
+  def new
+
+  end
+
+  def create
+    @student = Student.find(session[:student_id]) if session[:student_id]
+
+    if @student
+
+    else
+
+    end
+  end
+
+  def destroy
+
+  end
+
 end
